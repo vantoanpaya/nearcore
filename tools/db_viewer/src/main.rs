e0a00188f1f71c9e415225d73b7fa3fa05c52051
Marcin Michalski <michalski@near.org>
2022-03-29 14:47:19 +0200

    scan more

--- tools/db_viewer/src/main.rs
+++ tools/db_viewer/src/main.rs
@@ -17,11 +17,22 @@ fn main() {
     let store = Store::new(db);
     let tip: Tip = store.get_ser(ColBlockMisc, HEAD_KEY).unwrap().unwrap();
 
-    for i in 1..1000 {
+    for i in 1..100000 {
+        if i == tip.height {
+            break;
+        }
         let key = index_to_bytes(tip.height - i);
 
         let value: Option<HashMap<EpochId, HashSet<CryptoHash>>> =
             store.get_ser(ColBlockPerHeight, &key).unwrap();
-        println!("{} Value: {:?}", tip.height - i, value);
+
+        if let Some(x) = value {
+            if x.len() > 1 || x.iter().any(|f| f.1.len() > 1) {
+                println!("{} Value: {:?}", tip.height - i, x);
+            }
+        }
+        if i % 1000 == 0 {
+            println!("{} {}", i, tip.height - i);
+        }
     }
 }
